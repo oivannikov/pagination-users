d3e35a728409ddcae0e9541caecea1099fee3e33
oivannikov <alex0502ivannikov@gmail.com>
2020-11-15 01:21:52 +0200

    fix request

--- src/api/users.js
+++ src/api/users.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = "http://77.120.241.80:8811/api";
+const BASE_URL = "//77.120.241.80:8811/api";
 
 export async function getUsers() {
   const response = await axios.get(`${BASE_URL}/users`);
